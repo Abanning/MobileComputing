860f9117439045a69bf850b015a44c5ae0c5d270
Alex Banning <ajbanning@gmail.com>
2024-10-03 14:41:30 -0700

    HW1-Hello World app with basic navigation implementation

---
+++ HW1/HW1Tests/HW1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  HW1Tests.swift
+//  HW1Tests
+//
+//  Created by Alex Banning on 10/1/24.
+//
+
+import Testing
+@testable import HW1
+
+struct HW1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
